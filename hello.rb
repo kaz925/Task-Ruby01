810455df01f497b7014347b3ed9095f8f3f1be6d
EC2 Default User <nisi_kazu_of_world@yahoo.co.jp>
2020-11-08 06:39:58 +0000

    課題提出-Ruby02

--- hello.rb
+++ hello.rb
@@ -1,8 +1,42 @@
-p "Branding Engineer"
-p "Hello"
-p "HR Tech"
+#1 
+puts "初めてのRuby"
 
-puts 1
-puts 2
-puts 3
+#2
+puts "Ruby"+"始めました"
 
+#3
+puts 3+3
+puts 3-3
+puts 3*3
+puts 3/3
+
+#4#5#6
+num_array = [1,2,3,4]
+
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+
+#7
+kaz = {name:"kazuya nishizawa", brithday:925, bloodtype:"O"}
+
+p kaz[:name]
+p kaz[:brithday]
+p kaz[:bloodtype]
+
+#8
+def plus_ruby(name)
+  (name)+"ruby"
+end
+
+p plus_ruby("like")
+p plus_ruby("yhee")
+
+#9
+def plus_one(name)
+  (name)+1
+end
+
+p plus_one(4)
+p plus_one(10)
